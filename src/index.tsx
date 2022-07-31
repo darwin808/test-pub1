cacdf6300549edab9d6ebf4692e07222e30f41ef
darwin808 <darwin.apolinario12@gmail.com>
2022-07-31 16:08:23 +0800

    feat(index): s

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react"
 import { ZestyExplorer } from "./components"
 import { createZestyDiv } from "utils"
 import * as helper from "utils/index"
+import ReactDOM from "react-dom"
 // import { dummydata } from "constants"
-import { render } from "react-dom"
+// import { render } from "react-dom"
 // import { dummy2, dummydata } from "constants"
 
 // if (process.env.NODE_ENV === "production") {
@@ -12,16 +13,23 @@ import { render } from "react-dom"
 //    console.debug = () => {}
 // }
 
-console.log(" ********** Zesty live editor loaded v898888 ************ ")
+console.log(
+   " ********** Zesty live editor loaded 11111111111111111111111111111111 ************ ",
+)
 
 export const ZestyLiveEditor = (content?: any) => {
-   if (!helper.canUseDOM()) {
-      return null
-   }
+   // if (!helper.canUseDOM()) {
+   //    return null
+   // }
    createZestyDiv()
 
-   const container = document.getElementById("zesty-explorer")
-   render(<ZestyExplorer content={content} />, container)
+   // const container = document.getElementById("zesty-explorer")
+   // render(<ZestyExplorer content={content} />, container)
+
+   ReactDOM.render(
+      <ZestyExplorer content={content} />,
+      document.getElementById("zesty-explorer"),
+   )
 }
 
 // process.env.NODE_ENV === "development" ? ZestyLiveEditor(dummydata) : ZestyLiveEditor()
